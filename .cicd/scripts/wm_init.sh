c7eaf3585bfee3df80f8ccdd12a51efca47f622d
todd arbetter <todd.arbetter@noaa.gov>
2026-08-24 13:15:20 -0600

    Remove GitHub issue label commands from wm_init.sh
    Removed echo and curl commands for GitHub issue labels.

--- .cicd/scripts/wm_init.sh
+++ .cicd/scripts/wm_init.sh
@@ -44,6 +44,3 @@ GIT_REPO_NAME=$(echo "$GIT_URL" | cut -d '/' -f5 | cut -d '.' -f1)
 echo "$GIT_OWNER"
 echo "GIT_REPO_NAME"
 echo "$CHANGE_ID"
-
-echo "https://api.github.com/repos/${GIT_OWNER}/${GIT_REPO_NAME}/issues/${CHANGE_ID}/labels/${UFS_PLATFORM}-CI_RUNNING"
-curl --silent -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer ${GITHUB_TOKEN}"  https://api.github.com/repos/"${GIT_OWNER}"/"${GIT_REPO_NAME}"/issues/"${CHANGE_ID}"/labels -d '{"labels":["'"$UFS_PLATFORM"'-CI_RUNNING"]}'
